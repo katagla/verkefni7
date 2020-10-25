9bcadd40cb386455bd11d02f2d03070bbc2df0b3
katagla <kat21@hi.is>
2020-10-25 17:24:23 +0000

    gera decode og encode föll, byrja á start

--- scripts.js
+++ scripts.js
@@ -3,13 +3,23 @@
  */
 
 const LETTERS = `AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ`;
+const alphabet = LETTERS.split('');
 
 /**
  * Byrja forrit.
  */
 function start() {
-  prompt('Hvort viltu kóða eða afkóða streng? Skrifaðu „kóða“ eða „afkóða“')
-  if($input="kóða") {;}
+  let begin =prompt('Hvort viltu kóða eða afkóða streng? Skrifaðu „kóða“ eða „afkóða“');
+  if(begin.localeCompare("kóða")!=0 && begin.localeCompare("afkóða")!=0) {
+    alert("Veit ekki hvaða aðgerð " + begin + " er. Reyndu aftur");
+    start();
+  }
+  let n=prompt('Hversu mikið á að hliðra streng? Gefðu upp heiltölu á bilinu [1,31]');
+  if(!Number.isInteger(n) && n<1 && n>31) {
+    alert(n + " er ekki heiltala á bilinu [1, 31]. Reyndu aftur.");
+    start();
+  }
+  let str=prompt('Gefðu upp strenginn sem á að '+ begin +' með hliðrun ' +n);
 }
 
 // Hér er gott að commenta út til að vinna í encode/decode föllum fyrst og síðan „viðmóti“ forrits
@@ -23,9 +33,17 @@ start();
  * @returns {string} Upprunalegi strengurinn hliðraður um n til hægri
  */
 function encode(str, n) {
+  str=str.toLocaleUpperCase();
+  str=str.split('');
+  for(i=0;i<str.length; i++) {
+    let position=alphabet.indexOf(str[i]);
+    let newPosition=(position+n)%alphabet.length;
+    str[i]=alphabet[newPosition];
+  }
+  str=str.join('');
   return str;
 }
-
+console.log(encode('kata',3));
 /**
  * Afkóðar streng með því að hliðra honum um n stök.
  *
@@ -34,9 +52,23 @@ function encode(str, n) {
  * @returns {string} Upprunalegi strengurinn hliðraður um n til vinstri
  */
 function decode(str, n) {
+  str=str.toLocaleUpperCase();
+  str=str.split('');
+  for(i=0;i<str.length; i++) {
+    let position=alphabet.indexOf(str[i]);
+    let newPosition=position;
+    if (position-n<0) {
+      newPosition=(position+alphabet.length-n)%alphabet.length;
+    }
+    else { 
+      newPosition=(position-n)%alphabet.length;
+    }
+    str[i]=alphabet[newPosition];
+  }
+  str=str.join('');
   return str;
 }
-
+console.log(decode('DÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖAÁB',3));
 console.assert(encode('A', 3) === 'D', 'kóðun á A með n=3 er D');
 console.assert(decode('D', 3) === 'A', 'afkóðun á D með n=3 er A');
 console.assert(encode('AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ', 32) === 'AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ', 'kóðun með n=32 er byrjunarstrengur');
